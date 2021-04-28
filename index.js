a6b8920f616d49b35085c71757191cce9ffbdeed
andre <andre_w3@yahoo.com>
2021-04-27 22:30:37 -0400

    working on modal

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let modal = document.getElementById("playBtn");
+let modal = document.getElementById("playModal");
 
 let btn = document.getElementById("playBtn");
 
@@ -6,14 +6,17 @@ let span = document.getElementsByClassName("close")[0];
 
 btn.onClick = function () {
     modal.style.display = "block";
+    console.log("you clicked btn");
 }
 
 span.onClick = function () {
     modal.style.display = "none";
+    console.log("you clicked span");
 }
 
 window.onClick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+    console.log("you clicked window");
 }
